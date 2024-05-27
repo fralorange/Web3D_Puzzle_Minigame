d040b6c508b3d98c5dd174b1533bda9f74ffed70
fralorange <fralorange@mail.ru>
2024-05-27 20:53:32 +0300

    Implement 1/2 of Base Object positions

--- src/js/app.js
+++ src/js/app.js
@@ -12,13 +12,15 @@ document.body.appendChild(renderer.domElement);
 
 // cam
 const controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 0, 5);
+camera.position.set(75, 15, 0);
 controls.update();
 
 // light
 const light = new THREE.DirectionalLight(0xFFFFFF, 1);
 light.position.set(1, 1, 1).normalize();
 scene.add(light);
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+scene.add(ambientLight);
 
 // scene
 scene.background = new THREE.Color(0xFFFFFF);
@@ -27,45 +29,55 @@ scene.background = new THREE.Color(0xFFFFFF);
 const loader = new STLLoader();
 const pieces = [];
 
-function loadModel(url, id, x, y, z) {
+function loadModel(url, id, x, y, z, rotationX, rotationY, rotationZ, scaleX, scaleY, scaleZ) {
 	loader.load(`../models/${url}.stl`, (geometry) => {
-		const material = new THREE.MeshStandardMaterial({ color: Math.random() * 0xFFFFFF });
+		const material = (url.includes('Part') || url === 'Rod') ? new THREE.MeshStandardMaterial({ color: 0xFF0000 }) : new THREE.MeshStandardMaterial({ color: 0x00FF00 });
 		const model = new THREE.Mesh(geometry, material);
+		model.visible = false;
 		model.position.set(x, y, z);
+		model.rotation.set(rotationX, rotationY, rotationZ);
+		model.scale.set(scaleX, scaleY, scaleZ);
 		model.userData.id = id;
 		scene.add(model);
 		pieces.push(model);
 	});
 }
 
+function duplicateAndMirrorModel(model) {
+	const mirroredModel = model.clone();
+	mirroredModel.position.x = -model.position.x;
+	mirroredModel.rotation.y = Math.PI;
+	return mirroredModel;
+}
+
 const models = [
-	{ url: 'Part1', id: 1, x: 0, y: 0, z: 0 },
-	{ url: 'Part2', id: 2, x: 1.5, y: 0, z: 0 },
-	{ url: 'Part3', id: 3, x: 3, y: 0, z: 0 },
-	{ url: 'Part4', id: 4, x: 4.5, y: 0, z: 0 },
-	{ url: 'Part5', id: 5, x: 6, y: 0, z: 0 },
-	{ url: 'Part6', id: 6, x: 7.5, y: 0, z: 0 },
-	{ url: 'Part7', id: 7, x: 9, y: 0, z: 0 },
-	{ url: 'Part8', id: 8, x: 10.5, y: 0, z: 0 },
-	{ url: 'Part9', id: 9, x: 12, y: 0, z: 0 },
-	{ url: 'Part10', id: 10, x: 13.5, y: 0, z: 0 },
-	{ url: 'Part11', id: 11, x: 15, y: 0, z: 0 },
-	{ url: 'Part12', id: 12, x: 16.5, y: 0, z: 0 },
-	{ url: 'Part13', id: 13, x: 18, y: 0, z: 0 },
-	{ url: 'Part14', id: 14, x: 19.5, y: 0, z: 0 },
-	{ url: 'Part15', id: 15, x: 21, y: 0, z: 0 },
-	{ url: 'Part16', id: 16, x: 22.5, y: 0, z: 0 },
-	{ url: 'Part17', id: 17, x: 24, y: 0, z: 0 },
-	{ url: 'Part18', id: 18, x: 25.5, y: 0, z: 0 },
-	{ url: 'Part19', id: 19, x: 27, y: 0, z: 0 },
-	{ url: 'Part20', id: 20, x: 28.5, y: 0, z: 0 },
-	{ url: 'Part21', id: 21, x: 30, y: 0, z: 0 },
-	{ url: 'Stem', id: 22, x: 31.5, y: 0, z: 0 },
-	{ url: 'Rod', id: 23, x: 33, y: 0, z: 0 },
+	{ url: 'Part1', id: 1, x: 0, y: 0, z: 0, rotationX: 0, rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part2', id: 2, x: 1.1, y: -2.5, z: -4, rotationX: (11*Math.PI / 6), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part3', id: 3, x: 1.2, y: 7, z: 1.5, rotationX: (Math.PI / 2.1), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1.25, scaleZ: 1 },
+	{ url: 'Part4', id: 4, x: 2.2, y: -3, z: 12, rotationX: (Math.PI / 1.25), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part5', id: 5, x: 2.3, y: -3, z: -16, rotationX: -(Math.PI * 1.1), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part6', id: 6, x: 3, y: 15.7, z: 0.6, rotationX: (Math.PI / 5.6), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part7', id: 7, x: 3.3, y: -16.5, z: 5, rotationX: (Math.PI * 1.1), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part8', id: 8, x: 5.2, y: -4, z: 15.5, rotationX: -(Math.PI / 1.8), rotationY: 0, rotationZ: (Math.PI), scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part9', id: 9, x: 4, y: -5, z: -10, rotationX: (Math.PI / 1.15), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part10', id: 10, x: 4.4, y: -7, z: 13, rotationX: (Math.PI / 1.35), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part11', id: 11, x: 6.4, y: 18, z: -6, rotationX: (2* Math.PI / 0.98), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part12', id: 12, x: 5.6, y: -5, z: -11, rotationX: (Math.PI / 1.8), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part13', id: 13, x: 7.4, y: -12, z: 12, rotationX: (Math.PI / 1.1), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part14', id: 14, x: 7.0, y: -2, z: 12, rotationX: (Math.PI / 1.75), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part15', id: 15, x: 7.4, y: -5, z: -19, rotationX: -(Math.PI / 2), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part16', id: 16, x: 8.8, y: 18, z: 0, rotationX: 0, rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part17', id: 17, x: 9.4, y: -10, z: -6.5, rotationX: -(Math.PI / 1.35), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part18', id: 18, x: 11.4, y: -15, z: -5.5, rotationX: (Math.PI * 1.1), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part19', id: 19, x: 13.4, y: 8.5, z: 15, rotationX: (Math.PI / 3), rotationY: (Math.PI), rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part20', id: 20, x: 11.4, y: 7.5, z: -10, rotationX: 0, rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Part21', id: 21, x: 12.4, y: 2, z: -6, rotationX: (Math.PI * 1.15), rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Stem', id: 22, x: 31.5, y: 0, z: 0, rotationX: 0, rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
+	{ url: 'Rod', id: 23, x: 33, y: 0, z: 0, rotationX: 0, rotationY: 0, rotationZ: 0, scaleX: 1, scaleY: 1, scaleZ: 1 },
 ];
 
 models.forEach(async (model) => {
-	loadModel(model.url, model.id, model.x, model.y, model.z);
+	loadModel(model.url, model.id, model.x, model.y, model.z, model.rotationX, model.rotationY, model.rotationZ, model.scaleX, model.scaleY, model.scaleZ);
 	const modelStl = await import(`../models/${model.url}.stl`);
 });
 
@@ -93,7 +105,7 @@ order.forEach(id => {
 				piece.visible = true;
 			}
 		} else {
-			alert(`Неправильно!`);
+			alert(`Неверный выбор!`);
 		}
 	});
 	menu.appendChild(button);
